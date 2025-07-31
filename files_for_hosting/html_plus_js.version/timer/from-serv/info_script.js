961bea96664bddd0bda7b1fb80316062216cfadc
VobiCode <sasasuhinin0845@gmail.com>
2025-07-31 23:58:47 +0300

    upgraded timer + maintenance

---
+++ files_for_hosting/html_plus_js.version/timer/from-serv/info_script.js
@@ -0,0 +1,3 @@
+
+const API_KEY = 'AIzaSyD-sAf0Wiw7gEYybgMeh5zIZHRKOxNPZ1M'; // не мой кста, можете не пробовать
+const CHANNEL_ID = 'UCACwRQLU0Bq5yxijfnBPRfQ';
--- files_for_hosting/html_plus_js.version/timer/info_script.js
+++ files_for_hosting/html_plus_js.version/timer/info_script.js
@@ -1,3 +1,8 @@
-const API_KEY = 'ENTER_API_KEY';
-const AUTH_PASSWORD = 'ENTER_AUTH_PASSWORD'; 
-const CHANNEL_ID = 'ENTER_CHANNEL_ID'; //
+
+// При ошибке 403 проверьте настройки API ключа в Google Cloud Console:
+// 1. Application restrictions: None или HTTP referrers с вашим доменом
+// 2. API restrictions: YouTube Data API v3 должен быть включен
+// 3. Quotas: дневной лимит не превышен
+const API_KEY = 'AIzaSyAKg7uinfJrhSNdbAxGBCN7tRVbaqWF4lY'; // not mine
+const AUTH_PASSWORD = 'PS'; 
+const CHANNEL_ID = 'UCACwRQLU0Bq5yxijfnBPRfQ';
