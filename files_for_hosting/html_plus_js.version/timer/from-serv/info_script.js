d8736ffda5f89dc1e6219a05b7d54e0b5a8f3443
VobiCode <sasasuhinin0845@gmail.com>
2025-08-01 00:00:00 +0300

    upd for the last commit

--- files_for_hosting/html_plus_js.version/timer/from-serv/info_script.js
+++
@@ -1,3 +0,0 @@
-
-const API_KEY = 'AIzaSyD-sAf0Wiw7gEYybgMeh5zIZHRKOxNPZ1M'; // не мой кста, можете не пробовать
-const CHANNEL_ID = 'UCACwRQLU0Bq5yxijfnBPRfQ';
--- files_for_hosting/html_plus_js.version/timer/info_script.js
+++ files_for_hosting/html_plus_js.version/timer/info_script.js
@@ -1,8 +1,3 @@
-
-// При ошибке 403 проверьте настройки API ключа в Google Cloud Console:
-// 1. Application restrictions: None или HTTP referrers с вашим доменом
-// 2. API restrictions: YouTube Data API v3 должен быть включен
-// 3. Quotas: дневной лимит не превышен
 const API_KEY = 'AIzaSyAKg7uinfJrhSNdbAxGBCN7tRVbaqWF4lY'; // not mine
 const AUTH_PASSWORD = 'PS'; 
 const CHANNEL_ID = 'UCACwRQLU0Bq5yxijfnBPRfQ';
